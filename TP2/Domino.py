76eba0a5b43ec17eb18801b3eab2c7d87f92886a
BenjamBrink <72279529+BenjamBrink@users.noreply.github.com>
2020-10-05 16:24:05 +0200

    Add files via upload

--- TP2/Domino.py
+++ TP2/Domino.py
@@ -5,15 +5,18 @@
 
 """
 
+
 class Domino:
     #   We give the two numbers on both halves of the Domino
     right = None
     left = None
 
-    #   First, we define all functions needed
+    #   First, we define all static methods needed
+    @staticmethod
     def print_line():
         return "     +-----|-----+"
 
+    @staticmethod
     def create_str_domino(number):
         if number == 0:
             line_1 = '     '
@@ -45,19 +48,20 @@ class Domino:
             line_3 = '*   *'
         return line_1, line_2, line_3
 
-    def print_one_domino(self):
+    @staticmethod
+    def print_one_domino(domino):
         # We generate the lines of the domino
         # This function gets a Domino object plugged in
-        left_line_1, left_line_2, left_line_3 = create_str_domino(self.left)
-        right_line_1, right_line_2, right_line_3 = create_str_domino(self.right)
+        left_line_1, left_line_2, left_line_3 = Domino.create_str_domino(domino.left)
+        right_line_1, right_line_2, right_line_3 = Domino.create_str_domino(domino.right)
         line_1 = '     |{0}|{1}|'.format(left_line_1, right_line_1)
-        line_2 = '     |{1}|{2}|'.format(left_line_2, right_line_2)
+        line_2 = '     |{0}|{1}|'.format(left_line_2, right_line_2)
         line_3 = '     |{0}|{1}|'.format(left_line_3, right_line_3)
 
         printed_lines = [line_1, line_2, line_3]
 
         return printed_lines
-      
+
     #   Special Methods
 
     #   Returns "Domino(X, Y)"
@@ -69,11 +73,11 @@ class Domino:
     def __str__(self):
 
         #   Then, we use them to return the string we need
-        return (print_line() + "\n" +
-                print_one_domino(self)[0] + "\n" +
-                print_one_domino(self)[1] + "\n" +
-                print_one_domino(self)[2] + "\n" +
-                print_line()
+        return (Domino.print_line() + "\n" +
+                Domino.print_one_domino(self)[0] + "\n" +
+                Domino.print_one_domino(self)[1] + "\n" +
+                Domino.print_one_domino(self)[2] + "\n" +
+                Domino.print_line()
                 )
 
     #   Other Special Methods
@@ -88,4 +92,4 @@ class Domino:
             return False
 
     def __ne__(self, other):
-        return not(Domino.__eq__(self, other))
+        return not (Domino.__eq__(self, other))
